acca2be777824fe755232f91058b848b6572cea6
Abdelrahman Mostafa Kamel <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2025-06-07 04:24:46 +0300

    Add GEMINI_API_KEY to ProcessEnv interface

--- env.d.ts
+++ env.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
     // Extend the NodeJS ProcessEnv interface to include environment variables
     interface ProcessEnv {
         GITHUB_TOKEN: string;
+        GEMINI_API_KEY: string;
     }
 }
