4212afd9868a41a36543300d9c7a4ed26eaee94e
Abdelrahman Mostafa Kamel <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2025-06-09 01:54:48 +0300

    Refactor Chatbot component by separate MessageContainer and ChatbotForm, improving structure and readability

--- components/chatbot/Chatbot.tsx
+++ components/chatbot/Chatbot.tsx
@@ -7,7 +7,6 @@ import React, {
     useRef,
     useState,
 } from "react";
-import CloseIcon from "@mui/icons-material/Close";
 import ChatIcon from "@mui/icons-material/Chat";
 import RestaurantIcon from "@mui/icons-material/Restaurant";
 import { z } from "zod";
@@ -17,49 +16,37 @@ import { sendMessages } from "@/server/bot.actions";
 import { Message } from "./Message";
 import { ChatbotFloatingButton } from "./ChatbotFloatingButton";
 import ArrowCircleDownRoundedIcon from "@mui/icons-material/ArrowCircleDownRounded";
+import {
+    BotChatHistoryType,
+    MessageSchema,
+    UserChatHistoryType,
+} from "@/types/bot.types";
 import { scrollDown, updateBottomReachedHandler } from "@/lib/scrolling";
+import { ChatbotHeader } from "./ChatbotHeader";
 
-const ChatbotHeader = ({ closeChat }: { closeChat: () => void }) => (
-    <div className="flex justify-between items-center mb-2">
-        <h2 className="text-lg font-semibold text-indigo-600">Chat with us</h2>
-        <button
-            onClick={closeChat}
-            className="text-gray-500 hover:text-gray-700 h-fit rounded-full flex items-center justify-center cursor-pointer">
-            <CloseIcon />
-        </button>
-    </div>
-);
-
-const MessageSchema = z.object({
-    message: z
-        .string()
-        .min(1, "Message cannot be empty")
-        .max(100, "Message is too long"),
-});
+import { MessageContainer } from "./MessageContainer";
+import { ChatbotForm } from "./ChatbotForm";
 
 export default function Chatbot() {
     const [isOpen, setIsOpen] = useState(false);
+    const form = useForm<z.infer<typeof MessageSchema>>({
+        resolver: zodResolver(MessageSchema),
+    });
     const {
         register,
         handleSubmit,
         formState: { errors, isValid, isSubmitting },
-    } = useForm<z.infer<typeof MessageSchema>>({
-        resolver: zodResolver(MessageSchema),
-    });
-    const [botChatHistory, setBotChatHistory] = useState<
-        { message: ReactNode; time: string; isBot: boolean }[]
-    >([
+    } = form;
+    const [botChatHistory, setBotChatHistory] = useState<BotChatHistoryType>([
         {
             message: "Hello! How can I assist you today?",
-            time: "11:45 AM",
             isBot: true,
         },
     ]);
-    const [userChatHistory, setUserChatHistory] = useState<
-        { message: string; time: string; isBot: boolean }[]
-    >([]);
+    const [userChatHistory, setUserChatHistory] = useState<UserChatHistoryType>(
+        []
+    );
     const [isBottomReached, setIsBottomReached] = useState(true);
-
     const MessageContainerRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
@@ -77,36 +64,44 @@ export default function Chatbot() {
                 );
             };
         }
-    }, [MessageContainerRef.current, botChatHistory, userChatHistory]);
+    }, [MessageContainerRef.current, userChatHistory]);
 
     const onSubmit = async (data: z.infer<typeof MessageSchema>) => {
-        const time = new Date().toLocaleTimeString([], {
-            hour: "2-digit",
-            minute: "2-digit",
-        });
         const newUserHistory = [
             ...userChatHistory,
-            { message: data.message, time, isBot: false },
+            { message: data.message, isBot: false },
         ];
         setUserChatHistory(newUserHistory);
-        const response = await sendMessages(
-            newUserHistory.map((chat) => ({
-                role: "user",
-                parts: [{ text: chat.message }],
-            }))
-        );
-        setBotChatHistory((prev) => [
-            ...prev,
-            { message: response || "Server is busy", time, isBot: true },
-        ]);
+
+        try {
+            const response = await sendMessages(
+                newUserHistory.map((chat) => ({
+                    role: "user",
+                    parts: [{ text: chat.message }],
+                }))
+            );
+            setBotChatHistory((prev) => [
+                ...prev,
+                {
+                    message: response,
+                    isBot: true,
+                },
+            ]);
+        } catch (error) {
+            console.error("Error sending message:", error);
+            setBotChatHistory((prev) => [
+                ...prev,
+                {
+                    message: "An error occurred. Please try again.",
+                    isBot: true,
+                },
+            ]);
+            return;
+        }
     };
     return (
         <>
-            <ChatbotFloatingButton
-                openChatbot={() => {
-                    setIsOpen(true);
-                }}
-            />
+            <ChatbotFloatingButton openChatbot={() => setIsOpen(true)} />
             <div
                 className={cn(
                     `fixed max-w-80 bg-white rounded-lg shadow-lg p-4 z-50 bottom-28 right-4 flex-col overflow-hidden`,
@@ -114,91 +109,22 @@ export default function Chatbot() {
                     "max-h-[calc(100vh-7rem)]", // to ensure it doesn't exceed viewport height
                     isOpen ? "flex" : "hidden"
                 )}>
-                <ChatbotHeader
-                    closeChat={() => {
-                        setIsOpen(false);
-                    }}
-                />
-
-                {/* messages container */}
-                <div
+                <ChatbotHeader closeChat={() => setIsOpen(false)} />
+                <MessageContainer
                     ref={MessageContainerRef}
-                    className={cn(
-                        "flex-1",
-                        "overflow-y-auto scrollbar-indigo",
-                        "border-t border-b space-y-2 border-gray-200", // separator lines
-                        "py-2 px-1 max-h-48", // limit height to fit within the chat window
-                        "relative" // for the scroll button
-                    )}>
-                    {Array.from({ length: botChatHistory.length }).map(
-                        (_, index) => (
-                            <React.Fragment key={index}>
-                                {botChatHistory[index] && (
-                                    <Message
-                                        message={botChatHistory[index].message}
-                                        time={botChatHistory[index].time}
-                                        isBot={botChatHistory[index].isBot}
-                                    />
-                                )}
-                                {userChatHistory[index] && (
-                                    <Message
-                                        message={userChatHistory[index].message}
-                                        time={userChatHistory[index].time}
-                                        isBot={userChatHistory[index].isBot}
-                                    />
-                                )}
-                            </React.Fragment>
-                        )
-                    )}
-                    {isSubmitting && (
-                        <Message message="thinking..." time=" " isBot />
-                    )}
-                </div>
-
-                <form
-                    className="mt-2 flex gap-2 items-center relative"
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        handleSubmit(onSubmit, (error) => {
-                            console.error(
-                                "Form submission error:",
-                                error.message?.message
-                            );
-                        })(e);
-                        if (isValid) {
-                            e.currentTarget.reset();
-                        }
-                    }}>
-                    <button
-                        type="button"
-                        className={cn(
-                            "absolute left-1/2 bottom-full -translate-y-4 -translate-x-1/2 h-fit w-fit z-50 transition-opacity",
-                            isBottomReached
-                                ? "opacity-0 pointer-events-none"
-                                : "opacity-100 pointer-events-auto"
-                        )}
-                        onClick={() => scrollDown(MessageContainerRef.current)}
-                        disabled={isBottomReached}
-                        aria-disabled={isBottomReached}
-                        aria-label="Scroll to bottom">
-                        <ArrowCircleDownRoundedIcon className="text-indigo-600 !text-2xl" />
-                    </button>
-                    <textarea
-                        {...register("message")}
-                        name="message"
-                        rows={2}
-                        placeholder="Type your message..."
-                        className="flex-1 border resize-none h-auto  border-gray-300 text-black rounded px-3 py-2 text-sm scrollbar-indigo"
-                    />
-                    <button
-                        disabled={isSubmitting}
-                        type="submit"
-                        className={
-                            "bg-indigo-600 text-white p-4 rounded-full disabled:bg-slate-300 hover:bg-indigo-700 transition flex items-center justify-center"
-                        }>
-                        <RestaurantIcon className="!text-lg" />
-                    </button>
-                </form>
+                    botChatHistory={botChatHistory}
+                    userChatHistory={userChatHistory}
+                    isSubmitting={isSubmitting}
+                    isBottomReached={isBottomReached}
+                />
+                <ChatbotForm
+                    {...form}
+                    register={register}
+                    handleSubmit={handleSubmit}
+                    onSubmit={onSubmit}
+                    isBottomReached={isBottomReached}
+                    scrollDown={() => scrollDown(MessageContainerRef.current)}
+                />
             </div>
         </>
     );
