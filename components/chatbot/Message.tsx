e57ee6cbee4c00e7e878939cbf019bef0f70cd1c
Abdelrahman Mostafa Kamel <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2025-06-09 01:47:58 +0300

    Refactor Message component to use MessageObjType

--- components/chatbot/Message.tsx
+++ components/chatbot/Message.tsx
@@ -3,29 +3,27 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { ReactNode } from "react";
 import ChefIcon from "../assets/ChefIcon";
 import { cn } from "@/lib/styles";
+import { MessageObjType } from "@/types/bot.types";
 
-export const Message = ({
-    message,
-    time,
-    isBot,
-}: {
-    message: ReactNode;
-    time: string;
-    isBot: boolean;
-}) => (
-    <div
-        className={cn("flex items-start gap-2.5", isBot || "flex-row-reverse")}>
-        {isBot ? (
-            <ChefIcon className="w-5 text-gray-700" />
-        ) : (
-            <AccountCircleIcon className="text-black !text-lg" />
-        )}
+export const Message = ({ messageObj }: { messageObj: MessageObjType }) => {
+    const { isBot, message } = messageObj;
+    return (
         <div
             className={cn(
-                "flex flex-col grow max-w-[180px] p-4 border-gray-200 bg-gray-100 rounded-xl",
-                isBot ? "rounded-ss-none" : "rounded-se-none"
+                "flex items-start gap-2.5",
+                isBot || "flex-row-reverse"
             )}>
-            {/* <div
+            {isBot ? (
+                <ChefIcon className="w-5 text-gray-700" />
+            ) : (
+                <AccountCircleIcon className="text-black !text-lg" />
+            )}
+            <div
+                className={cn(
+                    "flex flex-col grow max-w-[200px] p-4 border-gray-200 bg-gray-100 rounded-xl",
+                    isBot ? "rounded-ss-none" : "rounded-se-none"
+                )}>
+                {/* <div
                 className={cn(
                     "flex items-center space-x-2",
                     isBot ? "justify-start" : "justify-end"
@@ -37,7 +35,10 @@ export const Message = ({
                     {time}
                 </span>
             </div> */}
-            <div className="text-sm font-normal text-gray-900">{message}</div>
+                <div className="text-sm font-normal text-gray-900">
+                    {message}
+                </div>
+            </div>
         </div>
-    </div>
-);
+    );
+};
